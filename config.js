a96a97889d0fe61020ab0a153f8d7057681043a7
Idgt <idgt@idgt.me>
2025-06-28 05:04:58 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {} 
+
+
+// your telegram API credentials
+// please go to https://my.telegram.org to create your API credentials
+config.telegramApiId = 21283384;
+config.telegramApiHash = 'c5bfe4993178bfeaa223e41adc0fccdf';
+
+
+// server settings
+config.hostname = '127.0.0.1';
+config.port = 3000;
+
+
+module.exports = config;
